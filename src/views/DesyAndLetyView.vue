5049b2d2b936ad24ef4721d17c23ed9f94809820
itamarpambo <itamarpambo751@gmail.com>
2023-10-25 16:47:30 +0100

    adicionando figure na secção de serviço

--- src/views/DesyAndLetyView.vue
+++ src/views/DesyAndLetyView.vue
@@ -1,12 +1,34 @@
+<script>
+import { RouterLink } from 'vue-router'
+</script>
 <template>
-    <div id="desyAndLetyHomebanner">
-        <h1>Prestamos variados serviços de interesse feminino</h1>
-        <small>O seu conforto é a nossa prioriaded. Crie já sua conta!</small>
+    <div  id="desyAndLetyHomebanner">
+        <div class="banner-top">
+            <h1>Prestamos variados serviços de interesse feminino</h1>
+            <small>O seu conforto é a nossa prioridade. <RouterLink to="/">Confira já os nossos serviços!</RouterLink></small>
+    
+            <button>Saber mais...</button>
+        </div>
+        <main>
+            <center>
+                <h2>Temos um leque de serviços muito variados</h2>
 
-        <button>Saber mais...</button>
+                <figure>
+                    <div class="images-collection">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <figcaption>
+                        <h2>Desde os nossos serviços capilares...</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptates debitis facere minus amet, necessitatibus voluptatibus maiores explicabo sint ipsum, sunt laborum deserunt odio odit quaerat, enim quos illum maxime!
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel obcaecati deleniti quo alias ab, modi natus atque commodi suscipit rem eligendi provident magni facilis recusandae consequatur velit architecto repellendus molestiae!
+                        </p>
+                    </figcaption>
+                </figure>
+            </center>   
+        </main>
     </div>
-    <main>
-        
-    </main>
 </template>
 <style scoped>@import url('./DesyAndLetyView.css');</style>
